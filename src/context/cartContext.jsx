4b21625470792540cc1be718bd2b7e6cd189fbd6
SagarPrasad160 <sagarprasad916@gmail.com>
2023-05-23 18:54:05 +0530

    associate a user to cart item using authContext userLog state

--- src/context/cartContext.jsx
+++ src/context/cartContext.jsx
@@ -1,5 +1,7 @@
-import { createContext, useState, useEffect } from "react";
+import { createContext, useState, useEffect, useContext } from "react";
 import PropTypes from "prop-types";
+
+import authContext from "./authContext";
 
 // import { toast } from "react-toastify";
 import { db } from "../firestore/config";
@@ -27,11 +29,14 @@ export function CartProvider({ children }) {
     });
   }, []);
 
+  const { userLog } = useContext(authContext);
+
   const addToCart = async (item) => {
     const ref = collection(db, "carts");
     await addDoc(ref, {
       title: item.title,
       price: item.price,
+      uid: userLog.user.uid,
     });
   };
 
